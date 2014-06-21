4844c75ec2f17048bf127d60ff10c35fe0da3906
Corey Haines <coreyhaines@gmail.com>
2014-06-21 15:47:26 -0500

    More user apps

--- install.sh
+++ install.sh
@@ -13,22 +13,23 @@ git config --global user.name "Corey Haines"
 git config --global user.email "coreyhaines@gmail.com"
 
 echo "installing user apps"
-brew cask install google-chrome
-brew cask install steam
-brew cask install sizeup
-open /Users/coreyhaines/Applications/SizeUp.app
-brew cask install alfred
-open /Users/coreyhaines/Applications/Alfred\ 2.app
-brew cask install caffeine
-open /Users/coreyhaines/Applications/Caffeine.app
 brew cask install dropbox
 open /Users/coreyhaines/Applications/Dropbox.app
 echo "Make sure to set up what dropbox folders to selectively sync, so you can get 1Password vault faster"
+brew cask install alfred
+open /Users/coreyhaines/Applications/Alfred\ 2.app
+brew cask install google-chrome
+brew cask install sizeup
+open /Users/coreyhaines/Applications/SizeUp.app
+brew cask install caffeine
+open /Users/coreyhaines/Applications/Caffeine.app
 brew cask install onepassword
+echo "Wait for dropbox to sync before opening up onepassword"
+brew cask install steam
 
-# brew cask install macvim # appears to not work in 10.10
 
 echo "installing programming tools"
+# brew cask install macvim # appears to not work in 10.10
 brew cask install virtualbox
 brew cask install vagrant
 
